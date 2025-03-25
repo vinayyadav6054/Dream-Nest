16da8ea77049beb98fa232bb7cb2fb90bb9c357b
vinayyadav6054 <yvinay7271@gmail.com>
2025-03-25 09:29:15 +0530

    Login API and login UI functionality completed

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -64,4 +64,34 @@ router.post("/register", upload.single('profileImage'), async (req, res) =>{
     }
 })
 
+router.post("/login", async (req, res) =>{
+    try{
+        /* Take the information from the form */
+        const { email, password} = req.body;
+
+        /* Check if the User exists*/
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.status(409).json({message: "User doesn't exists!!"});
+        }
+
+        /* Compare the password with the hashed password */
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(400).json({message: "Invalid Credentials!!"});
+        }
+
+        /* Generate JWT token */
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
+        delete user.password;
+
+        res.status(200).json({ token, user });
+
+     }catch(err){
+        console.log(err)
+        res.status(500).json({error: err.message})
+     }
+})
+
 module.exports = router;
